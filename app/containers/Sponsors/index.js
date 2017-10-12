b50e88511592a452d27137c1ad534099fc487432
Audora Chalker <scribeklio@gmail.com>
2017-10-12 14:32:06 -0400

    padding fixes on space pg, sponsors page rough draft done

--- app/containers/Sponsors/index.js
+++ app/containers/Sponsors/index.js
@@ -19,7 +19,86 @@ export default class Sponsors extends React.PureComponent {
         <Helmet title="Sponsors" meta={[ { name: 'description', content: 'Description of Sponsors' }]}/>
         <Header />
         <div className="sponsorsBodyContainer"> 
-            
+
+        <div className="titleList"> 
+          <h2>Our Sponsors</h2>
+          <p className="aboutSponsorsText"> How much we love our sponsors </p>
+        </div>
+
+          <div className="sponsorList">
+              <div className="sponsorListing">
+                <div className="sponsorLogo"> 
+                  <img src="https://placeholdit.co//i/150x150" />
+                </div>
+
+                <div className="sponsorTextBlock"> 
+                  <h3> A company</h3>          
+                  <p>what they do</p>   
+                </div> 
+
+              </div> 
+
+              <div className="sponsorListing">
+              <div className="sponsorLogo"> 
+                <img src="https://placeholdit.co//i/150x150" />
+              </div>
+
+              <div className="sponsorTextBlock"> 
+                <h3> A company</h3>          
+                <p>what they do</p>   
+              </div> 
+
+            </div>
+
+            <div className="sponsorListing">
+            <div className="sponsorLogo"> 
+              <img src="https://placeholdit.co//i/150x150" />
+            </div>
+
+            <div className="sponsorTextBlock"> 
+              <h3> A company</h3>          
+              <p>what they do</p>   
+            </div> 
+
+          </div>
+
+               <div className="sponsorListing">
+                <div className="sponsorLogo"> 
+                  <img src="https://placeholdit.co//i/150x150" />
+                </div>
+
+                <div className="sponsorTextBlock"> 
+                  <h3> A company</h3>          
+                  <p>what they do</p>   
+                </div> 
+
+              </div>
+              
+              <div className="sponsorListing">
+              <div className="sponsorLogo"> 
+                <img src="https://placeholdit.co//i/150x150" />
+              </div>
+
+              <div className="sponsorTextBlock"> 
+                <h3> A company</h3>          
+                <p>what they do</p>   
+              </div> 
+
+            </div> 
+
+            <div className="sponsorListing">
+                <div className="sponsorLogo"> 
+                  <img src="https://placeholdit.co//i/150x150" />
+                </div>
+
+                <div className="sponsorTextBlock"> 
+                  <h3> A company</h3>          
+                  <p>what they do</p>   
+                </div> 
+
+              </div>    
+
+          </div>
         </div>
         <Footer />
       </div>
