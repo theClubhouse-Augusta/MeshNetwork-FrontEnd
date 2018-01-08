9a57466e8bd561fe1f4d9dcbe868f0f93d8edce2
Audora Chalker <scribeklio@gmail.com>
2018-01-08 15:52:56 -0500

    state-y dash stuff added, needs apis-onCommit & editor

--- app/components/EventDash/index.js
+++ app/components/EventDash/index.js
@@ -15,37 +15,65 @@ import './style.css';
 import './styleM.css';
 
 
-const getRowId = row => row.id; 
+const getRowId = row => row.id;
 
 export default class EventDash extends React.PureComponent {
   constructor(props) { 
     super(props); 
 
     this.state = {
-      //spaceEvents: [], 
       columns: [
-        { name:'status', title: 'Status', getCellValue: row => (row.event ? row.event.status : undefined)}, 
-        { name:'title', title: 'Event Name', getCellValue: row => (row.event ? row.event.title : undefined)},
-        //userID => user name as profile url? or email mailto? 
-        { name:'creator', title: 'Created By', getCellValue: row => (row.event ? row.event.creator : undefined)},
-        //array from eventorg api => Name list as links
-        { name:'organizers', title: 'Organizers', getCellValue: row => (row.event ? row.event.organizers : undefined)},
-        //pull event dates - make strings?? => string or array list
-        { name:'dateTime', title: 'Date(s) & Time(s)', getCellValue: row => (row.event ? row.event.dateTime : undefined)},
-        { name:'description', title: 'description', getCellValue: row => (row.event ? row.event.description : undefined)},
-      ], 
+        //attempting to keep name values === api obj keys             
+//ICON RENDERS 
+        //approved, denied
+        { name: 'status', title: 'Status' }, 
+        { name: 'title', title: 'Event Name'},
+//this is where we can't pull straight from api anymore 
+        //userID => query name 
+        { name: 'creator', title: 'Created By' },        
+        //was name added to event organizers either? 
+        { name: 'organizers', title: 'Organizers' }, 
+        // grab from whichever api & make human readable       
+        { name: 'dateTime', title: 'Date(s) & Time' },
+
+        //back to straight api 
+        { name: 'description', title: 'Description' },
+      ],
       rows: [
-        {event: {status: 1, title:'pynight', creator: 'sally sue', organizers: 'sally sue, bob bobert', dateTime: '1/12/2018 6p-9p', description: 'yay its python, as always newbie corner', }}, 
-      ], 
-      editingRows: [], 
-      addedRows: [], 
-      changedRows: [], 
-    }
-  };
+        {id: '1', status: '', title: 'PyNight', creator: '', organizers:'', dateTime: '', description: ''},
+        {id: '2', status: '', title: '1 mil cups', creator: '', organizers:'', dateTime: '', description: ''},
+        {id: '3', status: '', title: 'JS Meetup', creator: '', organizers:'', dateTime: '', description: ''},
+        
+      ],
+      editingRows: [],
+      addedRows: [],
+      changedRows: {},
+    };
+    this.changeAddedRows = this.changeAddedRows.bind(this);
+    this.changeEditingRows = this.changeEditingRows.bind(this);
+    this.changeChangedRows = this.changeChangedRows.bind(this);
+    this.commitChanges = this.commitChanges.bind(this);
+    this.onEditingRowsChange = this.onEditingRowsChange.bind(this); 
+  }
+
+  changeAddedRows(addedRows) {
+    this.setState({ addedRows });
+  }
+  changeEditingRows(editingRows) {
+    this.setState({ editingRows });
+  }
+  changeChangedRows(changedRows) {
+    this.setState({ changedRows });
+  }
+
+  onEditingRowsChange(editingRows) {
+    this.setState({
+      editingRows
+    });
+  }
 
 
-
-
+/*
   componentWillMount() {
     this.loadSpaceEvents(); 
   }
@@ -76,12 +104,15 @@ export default class EventDash extends React.PureComponent {
   getEventDateTime = () => { 
     fetch('http://localhost:8000/api/')
   }
+*/
 
+commitChanges({ added, changed, deleted }) {
 
-commitChanges() {} 
+}
 
   render() {
-    const { rows, columns } = this.state; 
+    const { rows, columns, editingRows, addedRows, changedRows } = this.state;
+
 
     return (
       <div className="eventDashContainer">
@@ -89,18 +120,22 @@ commitChanges() {}
           <Grid
             rows={rows} 
             columns={columns}
-            getRowId={getRowId}> 
+            getRowId={getRowId}
+            > 
             <EditingState
-              onCommitChanges={this.commitChanges} 
-            /> 
-            <Table />
-            <TableHeaderRow />
-            <TableEditRow />
-            <TableEditColumn 
-              showAddCommand
-              showEditCommand
-              showDeleteCommand
-            />
+              onCommitChanges={this.commitChanges}
+              editingRows={editingRows}
+              onEditingRowsChange={this.onEditingRowsChange}
+          />
+          <Table />
+          <TableHeaderRow />
+          <TableEditRow />
+          <TableEditColumn
+            showAddCommand
+            showEditCommand
+            showDeleteCommand
+            allowEditing={!editingRows.length}
+          />
           </Grid>
         </Paper>
       </div>
--- app/components/UserDash/index.js
+++ app/components/UserDash/index.js
@@ -7,37 +7,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper'; 
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui'; 
+import { EditingState } from '@devexpress/dx-react-grid'; 
+import { Grid, Table, TableHeaderRow, TableEditRow, TableEditColumn } from '@devexpress/dx-react-grid-material-ui'; 
+
 
 import './style.css';
 import './styleM.css';
 
+const getRowId = row => row.id;
 
 export default class UserDash extends React.PureComponent {
-  constructor(props) {
+  constructor(props) { 
     super(props); 
 
-    this.state = { 
+    this.state = {
       columns: [
-        { name: 'roleID', title: 'Role', getCellValue: row => (row.user ? row.user.roleID : undefined )},
-        { name: 'verfied', title: 'Verified', getCellValue: row => (row.user ? row.user.verified : undefined ) },
-        { name: 'name', title: 'Name', getCellValue: row => (row.user ? row.user.name : undefined ) },      
-        { name: 'email', title: 'Email', getCellValue: row => (row.user ? row.user.email : undefined ) },      
-        { name: 'created_at', title: 'Member Since', 
-        getCellValue: row => (row.user ? row.user.created_at : undefined ) },      
-        { name: 'ban', title: 'Banned?', getCellValue: row => (row.user ? row.user.ban : undefined )} ,
-      ], 
-      rows: [ 
-        {user: {roleID: '2', verified: '1', name: 'sally sue', email: 'sallys@gmail.com', created_at: 'Jan 3, 2018 16:00', ban: ''}}, 
-        {user: {roleID: '2', verified: '1', name: 'bob bobert', email: 'bobert@gmail.com', created_at: 'Jan 3, 2018 16:00', ban: ''}}, 
-        ], 
-      editingRows: [], 
-      addedRows: [], 
-      changedRows: [], 
-      deletingRows: [], 
-    }
-  } 
-// getCellValue is for if we want to assign the array of users to their own user objs 
+        //take & convert role ID
+        {name: 'role', title: 'Role'}, 
+        {name: 'verified', title: 'Verified'}, 
+        {name: 'email', title: 'Email'}, 
+        //??
+        {name: 'name', title: 'Name'}, 
+        //take & convert created_at
+        {name: 'dateTime', title: 'Member Since'},
+        {name: 'ban', title: ''}, 
+      ],
+//ROLEID => readable role name
+      rows: [
+        {id: '1', role: '', verified: '', email: '', name: 'bob bobert', dateTime: '', ban: ''},
+        {id: '2', role: '', verified: '', email: '', name: 'sally sue', dateTime: '', ban: ''},
+    
+        /* there is an option to wrap these in another object if need be
+        ie {user: {id:...}} you have to add  getCellValue: row => (row.user ? row.user.id : undefined) to the column dec*/
+      ],
+      editingRows: [],
+      addedRows: [],
+      changedRows: {},
+    };
+    this.changeAddedRows = this.changeAddedRows.bind(this);
+    this.changeEditingRows = this.changeEditingRows.bind(this);
+    this.changeChangedRows = this.changeChangedRows.bind(this);
+    this.commitChanges = this.commitChanges.bind(this);
+    this.onEditingRowsChange = this.onEditingRowsChange.bind(this); 
+  }
+
+  changeAddedRows(addedRows) {
+    this.setState({ addedRows });
+  }
+  changeEditingRows(editingRows) {
+    this.setState({ editingRows });
+  }
+  changeChangedRows(changedRows) {
+    this.setState({ changedRows });
+  }
+
+  onEditingRowsChange(editingRows) {
+    this.setState({
+      editingRows
+    });
+  }
 
 //roleID=> actual role name
 //created_at => readable date format
@@ -63,23 +91,44 @@ export default class UserDash extends React.PureComponent {
     }.bind(this)) 
 } */
 
-  render() { 
-    const { rows, columns} = this.state; 
-  
+
+commitChanges({ added, changed, deleted }) {
+
+}
+
+  render() {
+    const { rows, columns, editingRows, addedRows, changedRows } = this.state;
+
+
     return (
       <div className="userDashContainer">
         <Paper> 
           <Grid
-              rows={rows}
-              columns={columns}>
-              <Table />
-              <TableHeaderRow />
-           </Grid>
+            rows={rows} 
+            columns={columns}
+            getRowId={getRowId}
+            > 
+            <EditingState
+              onCommitChanges={this.commitChanges}
+              editingRows={editingRows}
+              onEditingRowsChange={this.onEditingRowsChange}
+          />
+          <Table />
+          <TableHeaderRow />
+          <TableEditRow />
+          <TableEditColumn
+            showAddCommand
+            showEditCommand
+            showDeleteCommand
+            allowEditing={!editingRows.length}
+          />
+          </Grid>
         </Paper>
       </div>
     );
   }
 }
+
 
 UserDash.contextTypes = {
   router: PropTypes.object
