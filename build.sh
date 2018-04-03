c7220b05e5f4ef5eda0f3a92141c944194075caa
Oscar-21 <austin.conder@outlook.com>
2018-04-03 01:37:40 -0400

    build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd "${my_backend}/public"
 rm -rf static/
 cd "${my_frontend}/build"
 cp -r . "${my_backend}/public"
-cd "${my_backend}/public"
+cd "${my_backend}"
 git add .
 git commit -m "build"
 eb deploy
