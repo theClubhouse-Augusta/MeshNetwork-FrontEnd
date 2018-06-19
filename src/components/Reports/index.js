50933c0c11b6921176d2874a73ac07f4873c33d1
Oscar-21 <austin.conder@outlook.com>
2018-06-19 13:26:45 -0400

    testing mobile link

--- src/components/Reports/index.js
+++ src/components/Reports/index.js
@@ -4,6 +4,7 @@ import reportStyles from '../../variables/styles/reportStyles';
 
 class Reports extends Component {
   state = {
+    hasSupportForDownloadAttr: false,
     verticals: [],
     logo: '',
     employeeCount: '',
@@ -25,6 +26,13 @@ class Reports extends Component {
     email: '',
     description: '',
     url: '',
+  };
+  setRef = element => {
+    if (element) {
+      if (typeof element.download !== "undefined") {
+        this.setState({ hasSupportForDownloadAttr: true });
+      }
+    }
   };
   // token = localStorage['token'];
   // componentDidMount() {
@@ -49,9 +57,25 @@ class Reports extends Component {
   //     } = company;
   //   }
   // };
+  //
+  //
+  makeTextFile = text => {
+    const textFile = `data:text/csv;charset=utf8,${encodeURI(text)}`
+    return textFile;
+  };
   render() {
+    const tf = this.makeTextFile("id,name,salary,start_date,depti\r\n1,Rick,623.3,2012-1-1,IT\r\n2,Dan,515.2,2013-9-23,Operations\r\n3,Michelle,611,2014-11-15,IT\r\n4,Ryan,729,2014-5-11,HR\r\n5,Gary,843.25,2015-3-27,Finance\r\n6,Nina,578,2013-5-21,IT\r\n7,Simon,632.8,2013-7-30,Operations\r\n8,Guru,722.5,2014-06-17,Finance");
     return (
-      <div>reports</div>
+      <div>
+        <a style={{ display: 'none' }} href="" ref={this.setRef}>hide</a>
+        {this.state.hasSupportForDownloadAttr && 
+          <a style={{ cursor: 'pointer', display: 'block' }} href={tf} download="woo.csv">pdf</a>
+        }
+        {!!this.state.hasSupportForDownloadAttr && 
+          <a style={{ cursor: 'pointer', display: 'block' }} href={tf}>pdf</a>
+        }
+        reports
+      </div>
     );
   }
 }
