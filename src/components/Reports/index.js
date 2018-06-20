ae3829f1f4b8d856511dd058519edb31504601bb
Oscar-21 <austin.conder@outlook.com>
2018-06-20 14:01:18 -0400

    testing reports again

--- src/components/Reports/index.js
+++ src/components/Reports/index.js
@@ -6,6 +6,7 @@ class Reports extends Component {
   state = {
     hasSupportForDownloadAttr: false,
     verticals: [],
+    id: '',
     logo: '',
     employeeCount: '',
     companyName: '',
@@ -26,7 +27,11 @@ class Reports extends Component {
     email: '',
     description: '',
     url: '',
+    companies: [],
+    linkReady: false,
+    columns: "name,email,address,city,state,zipcode,url,employeeCount,foundingDate\r\n",
   };
+  token = localStorage['token'];
   setRef = element => {
     if (element) {
       if (typeof element.download !== "undefined") {
@@ -34,45 +39,75 @@ class Reports extends Component {
       }
     }
   };
-  // token = localStorage['token'];
-  // componentDidMount() {
-  //   this.loadUser(this.token);
-  // };
-  // getCompany = async companyID => {
-  //   const response = await fetch(`http://localhost:8000/api/company/${companyID}`, {
-  //     headers: { Authorization: `Bearer ${localStorage['token']}` }
-  //   });
-  //   const json = await response.json()
-  //   const {
-  //     company,
-  //     verticals,
-  //     error,
-  //   } = json;
-  //   if (company) {
-  //     const {
-  //       logo,
-  //       employeeCount,
-  //       id: companyID,
-  //       name: companyName,
-  //     } = company;
-  //   }
-  // };
-  //
-  //
+  async componentDidMount() {
+    const companies = await this.getCompanies();
+    companies.forEach(async (companyId, index) => {
+      const lastItem = companies.length === (index + 1);
+      const currentCompany = await this.getCompany(companyId, lastItem);
+      if (currentCompany) {
+        console.log('001', this.state.columns);
+        this.setState(() => ({
+          columns: this.state.columns.concat(currentCompany),
+        }), () => {
+          console.log('002', this.state.columns);
+        });
+      }
+    });
+    this.setState(() => ({
+      linkReady: true,
+    }));
+  };
+  getCompany = async (companyID, lastItem) => {
+    const response = await fetch(`http://localhost:8000/api/company/${companyID}`, {
+      headers: { Authorization: `Bearer ${localStorage['token']}` }
+    });
+    const json = await response.json()
+    const {
+      company,
+      error,
+    } = json;
+    if (company) {
+      const {
+        employeeCount,
+        name: companyName,
+        email,
+        address,
+        city,
+        state,
+        zipcode,
+        foundingDate,
+        url,
+      } = company;
+      if (!lastItem) {
+        return `${companyName},${email},${address},${city},${state},${zipcode},${url},${employeeCount},${foundingDate}\r\n`;
+      } else if (lastItem) {
+        return `${companyName},${email},${address},${city},${state},${zipcode},${url},${employeeCount},${foundingDate}`;
+      }
+    } else if (error) {
+      return false;
+    }
+  };
+
+  getCompanies = async () => {
+    const response = await fetch(`http://localhost:8000/api/companies`);
+    const json = await response.json();
+    return json.companyIds;
+  };
+
   makeTextFile = text => {
     const textFile = `data:text/csv;charset=utf8,${encodeURI(text)}`
     return textFile;
   };
   render() {
-    const tf = this.makeTextFile("id,name,salary,start_date,depti\r\n1,Rick,623.3,2012-1-1,IT\r\n2,Dan,515.2,2013-9-23,Operations\r\n3,Michelle,611,2014-11-15,IT\r\n4,Ryan,729,2014-5-11,HR\r\n5,Gary,843.25,2015-3-27,Finance\r\n6,Nina,578,2013-5-21,IT\r\n7,Simon,632.8,2013-7-30,Operations\r\n8,Guru,722.5,2014-06-17,Finance");
+    const tf = this.state.linkReady ? this.makeTextFile(this.state.columns) : false;
     return (
       <div>
         <a style={{ display: 'none' }} href="" ref={this.setRef}>hide</a>
-        {this.state.hasSupportForDownloadAttr && 
+        {(this.state.hasSupportForDownloadAttr && this.state.linkReady) &&
           <a style={{ cursor: 'pointer', display: 'block' }} href={tf} download="woo.csv">pdf</a>
         }
-        {!!this.state.hasSupportForDownloadAttr && 
-          <a style={{ cursor: 'pointer', display: 'block' }} href={tf}>pdf</a>
+        {(!!!this.state.hasSupportForDownloadAttr && this.state.linkReady) &&
+          <a style={{ cursor: 'pointer', display: 'block' }} href={tf}>pdf2</a>
         }
         reports
       </div>
